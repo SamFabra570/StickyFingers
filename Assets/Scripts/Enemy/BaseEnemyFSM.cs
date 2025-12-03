f83ff2410d8dd685b7b54e8fd46004b401d94349
SamFabra <samfabra5@gmail.com>
2025-12-03 17:52:57 +0100

    enemy patrol added

--- Assets/Scripts/Enemy/BaseEnemyFSM.cs
+++ Assets/Scripts/Enemy/BaseEnemyFSM.cs
@@ -8,12 +8,12 @@ public class BaseEnemyFSM : MonoBehaviour
     public enum MindStates
     {
         kWait,
-        kSeek,
+        kPatrol,
         kPursuit,
         kAttack,
         kFlee
     }
-   
+    
     public MindStates current_mind_state_;
     public Sight sight_sensor_;
     public NavMeshAgent agent_;
@@ -22,8 +22,19 @@ public class BaseEnemyFSM : MonoBehaviour
 
     public float stop_attack_distance_multiplier = 1.2f;
 
-    public float stun_time_ = 2.0f;
+    //public float stun_time_ = 2.0f;
 
+    public GameObject fireEffect;
+    
+    public List<Transform> waypoints;
+    private Transform currentTarget;
+    private int index = 0;
+
+    private bool isMoving = true;
+    private bool atEnd = false;
+    private bool isReversing = false;
+
+    public float waitTime = 2.0f;
 
     private void Awake()
     {
@@ -33,19 +44,27 @@ public class BaseEnemyFSM : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        fireEffect.SetActive(false);
         
+        if (waypoints.Count > 0 && waypoints[0] != null)
+        {
+            //Set first target
+            currentTarget = waypoints[index];
+            
+            //Start moving towards first target
+            agent_.SetDestination(currentTarget.position);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         if(current_mind_state_ == MindStates.kWait)
         {
             MindWait();
-        }else if(current_mind_state_ == MindStates.kSeek)
+        }else if(current_mind_state_ == MindStates.kPatrol)
         {
-            MindSeek();
+            MindPatrol();
         }else if(current_mind_state_ == MindStates.kPursuit)
         {
             MindPursuit();
@@ -57,20 +76,64 @@ public class BaseEnemyFSM : MonoBehaviour
             MindFlee();
         }
 
+        
+    }
+
+    IEnumerator MoveToNextWaypoint()
+    {
+        if (!isReversing)
+        {
+            index++;
+        }
+        
+        //If not at last waypoint and not in reverse
+        if (index < waypoints.Count && !isReversing)
+        {
+            //Pause for set seconds before going to first waypoint
+            if (index == 1)
+                yield return new WaitForSeconds(waitTime);
+            
+            currentTarget = waypoints[index];
+        }
+        else
+        {
+            //If at last waypoint pause for set seconds
+            if (!atEnd)
+            {
+                atEnd = true;
+                yield return new WaitForSeconds(waitTime);
+            }
+
+            index--;
+            isReversing = true;
+
+            //If next waypoint is the first point, reset isReversing and atEnd
+            if (index == 0)
+            {
+                isReversing = false;
+                atEnd = false;
+            }
+            
+            currentTarget = waypoints[index];
+        }
+        
+        //Move to next waypoint
+        agent_.SetDestination(currentTarget.position);
+        isMoving = true;
     }
     
     void MindWait()
     {
         BodyWait();
         Debug.Log("Waiting");
-        //a�adir corrutina stun time
-        current_mind_state_ = MindStates.kSeek;
+        current_mind_state_ = MindStates.kPatrol;
 
     }
-    void MindSeek()
+    
+    void MindPatrol()
     {
-        BodySeek();
-        Debug.Log("Seeking");
+        BodyPatrol();
+        Debug.Log("Starting Patrol");
 
         if(sight_sensor_.detected_object_ != null)
         {
@@ -85,6 +148,7 @@ public class BaseEnemyFSM : MonoBehaviour
 
         if(sight_sensor_.detected_object_ == null)
         {
+            fireEffect.SetActive(false);
             current_mind_state_ = MindStates.kWait;
             return;
         }
@@ -97,6 +161,7 @@ public class BaseEnemyFSM : MonoBehaviour
         }
         
     }
+    
     void MindAttack()
     {
         Debug.Log("Attacking");
@@ -115,6 +180,7 @@ public class BaseEnemyFSM : MonoBehaviour
             current_mind_state_ = MindStates.kWait;
         }
     }
+    
     void MindFlee()
     {
         BodyFlee();
@@ -125,14 +191,24 @@ public class BaseEnemyFSM : MonoBehaviour
     {
         agent_.isStopped = true;
     }
-    void BodySeek()
+    
+    void BodyPatrol()
     {
-        //rotate o algo
-        if (current_mind_state_ == MindStates.kSeek)
+        //If in patrol state, start following waypoints
+        if (current_mind_state_ == MindStates.kPatrol && agent_.isStopped)
         {
-            Debug.Log("trying to turnnnnn");
-            transform.Rotate(new Vector3(0, 10, 0));
-
+            agent_.isStopped = false;
+            StartCoroutine(MoveToNextWaypoint());
+        }
+        
+        if (currentTarget != null)
+        {
+            //Check if agent has arrived at target position
+            if ((Vector3.Distance(transform.position, currentTarget.position) <= 2f) && isMoving)
+            {
+                isMoving = false;
+                StartCoroutine("MoveToNextWaypoint");
+            }
         }
         
     }
@@ -141,10 +217,12 @@ public class BaseEnemyFSM : MonoBehaviour
         if (agent_ != null && sight_sensor_.detected_object_ != null)
         {
             agent_.isStopped = false;
+            fireEffect.SetActive(true);
             agent_.SetDestination(sight_sensor_.detected_object_.transform.position);
         }
            
     }
+    
     void BodyAttack()
     {
         Vector3 dir = Vector3.Normalize(transform.forward);
@@ -154,6 +232,7 @@ public class BaseEnemyFSM : MonoBehaviour
         //atacar o algo
         //sight_sensor_.detected_object_.attachedRigidbody.AddForce(dir * 1500);
     }
+    
     void BodyFlee()
     {
 
