f18c841af0fc793626ee1f2f8fabf2222b433318
SamFabra <samfabra5@gmail.com>
2025-12-09 21:15:31 +0100

    added particle destroy
    need to fix spawn of smoke cloud still

--- Assets/Scripts/Smoke.cs
+++ Assets/Scripts/Smoke.cs
@@ -9,7 +9,14 @@ public class Smoke : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        blowSmoke = GetComponent<ParticleSystem>();
+        smokeCloud = GetComponent<ParticleSystem>();
+
+        var smokeMain = blowSmoke.main;
+        var cloudMain = smokeCloud.main;
         
+        smokeMain.stopAction = ParticleSystemStopAction.Destroy;
+        cloudMain.stopAction = ParticleSystemStopAction.Destroy;
     }
 
     // Update is called once per frame
@@ -21,16 +28,18 @@ public class Smoke : MonoBehaviour
     public void BlowSmoke(GameObject emitter, GameObject cloudPos, float waitTime)
     {
         blowSmoke.transform.SetPositionAndRotation(emitter.transform.position, emitter.transform.rotation);
+        
         blowSmoke.Play();
+        Debug.Log("Set cloud pos, NO SPAWN YET");
+        smokeCloud.transform.SetPositionAndRotation(cloudPos.transform.position, cloudPos.transform.rotation);
         StartCoroutine(SpawnCloud(cloudPos, waitTime));
     }
     
-    private IEnumerator SpawnCloud(GameObject pos, float time)
+    private IEnumerator SpawnCloud(GameObject cloudPos, float time)
     {
-        smokeCloud.transform.SetPositionAndRotation(pos.transform.position, pos.transform.rotation);
         yield return new WaitForSeconds(time);
 
-        Debug.Log("Spawn cloud");
+        Debug.Log("SPAWN DA CLOUD");
         smokeCloud.Play();
     }
 }
