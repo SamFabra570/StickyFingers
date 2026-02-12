ba69f9a6c9a32231b0a8be1c53e7e2e4a5086b84
SamFabra <samfabra5@gmail.com>
2026-02-12 03:08:49 +0100

    fixed hub ability selection bug

--- Assets/Scripts/HUB/UIAbilitySlot.cs
+++ Assets/Scripts/HUB/UIAbilitySlot.cs
@@ -4,11 +4,12 @@ using UnityEngine.EventSystems;
 
 public class UIAbilitySlot : MonoBehaviour, IDropHandler
 {
+    [SerializeField] private GameObject equippedAbility;
+    
     private void Update()
     {
         if (transform.childCount == 0)
             ResetAbilitySlot();
-            
     }
 
     public void OnDrop(PointerEventData eventData)
@@ -23,24 +24,27 @@ public class UIAbilitySlot : MonoBehaviour, IDropHandler
         }
     }
 
-    private void SetAbilitySlot(AbilitySlot ability)
+    private void SetAbilitySlot(AbilitySlot slot)
     {
         if (gameObject.CompareTag("Slot1"))
         {
-            AbilityManager.Instance.EquipAbility(0, ability);
-            Debug.Log("Ability 1 set");
+            AbilityManager.Instance.EquipAbility(0, slot);
+            //equippedAbility = abilityObject.gameObject;
+            Debug.Log("Ability 1 set" + slot.ability);
         }
             
         else if (gameObject.CompareTag("Slot2"))
         {
-            AbilityManager.Instance.EquipAbility(1, ability);
-            Debug.Log("Ability 2 set");
+            AbilityManager.Instance.EquipAbility(1, slot);
+            //equippedAbility = abilityObject.gameObject;
+            Debug.Log("Ability 2 set" + slot.ability);
         }
             
         else if (gameObject.CompareTag("Slot3"))
         {
-            AbilityManager.Instance.EquipAbility(2, ability);
-            Debug.Log("Ability 3 set: " + ability);
+            AbilityManager.Instance.EquipAbility(2, slot);
+            //equippedAbility = abilityObject.gameObject;
+            Debug.Log("Ability 3 set: " + slot.ability);
         }
     }
 
@@ -48,6 +52,18 @@ public class UIAbilitySlot : MonoBehaviour, IDropHandler
     {
         if (gameObject.CompareTag("Slot1"))
             AbilityManager.Instance.DequipAbility(0);
+        else if (gameObject.CompareTag("Slot2"))
+            AbilityManager.Instance.DequipAbility(1);
+        else if(gameObject.CompareTag("Slot3"))
+            AbilityManager.Instance.DequipAbility(2);
+        
+        //equippedAbility = null;
+    }
+
+    private void SetActiveAbilities()
+    {
+        if (gameObject.CompareTag("Slot1"))
+            Debug.Log("Ability 1 active");
         else if (gameObject.CompareTag("Slot2"))
             AbilityManager.Instance.DequipAbility(1);
         else if(gameObject.CompareTag("Slot3"))
