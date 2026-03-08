2a4c27f31fbabc0cfe3a0d457d89a5005c738ee8
Shmuel97 <110112690+Shmuel97@users.noreply.github.com>
2026-03-08 17:10:46 +0100

    Mage corrected

--- Assets/Scripts/MageSpawner.cs
+++ Assets/Scripts/MageSpawner.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
 public class MageSpawner : MonoBehaviour
 {
     public static  MageSpawner Instance;
@@ -8,6 +9,7 @@ public class MageSpawner : MonoBehaviour
     public Transform mageSpawn;
     [SerializeField] private GameObject magePrefab;
     [SerializeField] private GameObject mage;
+    public List<Transform> waypoints;
 
     private void Awake()
     {
@@ -21,6 +23,7 @@ public class MageSpawner : MonoBehaviour
     {
         GameObject prefab = Resources.Load<GameObject>("Mage");
         mage = Instantiate(prefab);
+        mage.GetComponent<BaseMageEnemy>().waypoints = waypoints;
         mage.transform.position = mageSpawn.position;
         
         UIManager.Instance.ShowMageSpawnNotif();
